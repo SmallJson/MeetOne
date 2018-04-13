1669882c6bccedbddd07aed3ed270a57ba472539
smartTu <tuyin1995@163.com>
2018-04-13 22:01:59 +0800

    构建项目

---
+++ src/main/java/com/bupt/meetone/MeetoneApplication.java
@@ -0,0 +1,12 @@
+package com.bupt.meetone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetoneApplication.class, args);
+	}
+}
